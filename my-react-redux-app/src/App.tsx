63fc7e033a19d66f06aac1d82062e9788ab89c5f
Ira-Bughaichuk <18IraBughaichuk@ukr.net>
2023-10-11 15:36:51 +0300

    add redux-ts

--- my-react-redux-app/src/App.tsx
+++ my-react-redux-app/src/App.tsx
@@ -1,33 +1,27 @@
-import React, {useState} from 'react';
-
+import React from 'react';
+import {useSelector, useDispatch} from 'react-redux';
+import {addTodo, deleteTodo} from './todoSlice';
+import {RootState} from './store';
 import './App.css';
 import AddTodoForm from './Components/AddTodoForm';
 import TodoItem from './Components/TodoItem';
 
-export interface Task {
-    id:number;
-    text: string;
-}
+// export interface Task {
+//     id:number;
+//     text: string;
+// }
 function App() {
-  const [todos, setTodos]= useState<Task[]>([]);
+  const todos = useSelector((state: RootState) => state.todos);
+  const dispatch = useDispatch()
 
-  const addTodo = (text:string) => {
-    const newTodo ={id: Date.now(), text};
-    setTodos([...todos, newTodo])
-  }
-
-  const deleteTodo = (id: number) => {
-    const newTodos = todos.filter(todo => todo.id !== id);
-    setTodos(newTodos);
-  }
   return (
     <div className="App">
       <h1>Todo List</h1>
-     <AddTodoForm onAdd={addTodo} />
+     <AddTodoForm onAdd={(text)=> dispatch(addTodo(text))} />
     
     <ul>
       {todos.map(todo => (
-        <TodoItem key={todo.id} todo={todo} onDelate={deleteTodo}/>
+        <TodoItem key={todo.id} todo={todo} onDelate={(id)=> dispatch(deleteTodo(id))}/>
       ))}
     </ul>
     </div>
